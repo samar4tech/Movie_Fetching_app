f853768884c9b214424538162f36afa07dac9c33
Samar Fatima <samar.khan7393@gmail.com>
2022-09-29 23:52:39 -0700

    Updates

---
+++ precache-manifest.369a7da2abcfebc07623e4db1bfc5444.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c32c5a685c24b499c023ef3e8d1c3f",
+    "url": "/Movie_Fetching_app/index.html"
+  },
+  {
+    "revision": "1727c7420d517949ff8b",
+    "url": "/Movie_Fetching_app/static/css/main.a71dfe66.chunk.css"
+  },
+  {
+    "revision": "b2d5c95ef3d7192cb466",
+    "url": "/Movie_Fetching_app/static/js/2.71ba1e20.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Movie_Fetching_app/static/js/2.71ba1e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1727c7420d517949ff8b",
+    "url": "/Movie_Fetching_app/static/js/main.ab6edd29.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Movie_Fetching_app/static/js/main.ab6edd29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abed9a0791672248ea34",
+    "url": "/Movie_Fetching_app/static/js/runtime-main.93826b1f.js"
+  }
+]);
